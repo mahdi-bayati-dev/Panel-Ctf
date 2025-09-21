d331444aa82e11ae614bb9cb44048a0e91d60025
Mahdi Bayati <mad.bayati@gmail.com>
2025-09-21 16:16:14 +0330

    edite-3

--- src/app/(protected)/user/[id]/page.jsx
+++ src/app/(protected)/user/[id]/page.jsx
@@ -114,6 +114,8 @@ export default function UserDetailPage() {
         year: "numeric",
         month: "long",
         day: "numeric",
+        hour: "2-digit", 
+        minute: "2-digit", 
       }),
     },
   ];
