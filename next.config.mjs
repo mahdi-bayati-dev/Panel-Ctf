5670fb4012251d18f02c60bbd9640db36170c010
Mahdi Bayati <mad.bayati@gmail.com>
2025-09-26 18:52:23 +0330

    Dbog-1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // کامنت: هر درخواستی که به /api/ در فرانت‌اند ارسال شود
+        source: "/api/:path*",
+        // کامنت: به صورت مخفیانه به سرور بک‌اند شما پراکسی (هدایت) می‌شود
+        destination: "https://api.eitebar.ir/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
